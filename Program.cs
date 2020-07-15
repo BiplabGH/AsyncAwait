fd0c218a859f54cc0775c860fd174f031c5a15d1
Biplab Sah <biplabsah@live.com>
2020-07-16 00:02:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumTwoOperationsAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task<int> SumTwoOperationsAsync()
+        {
+            var firstTask = GetOperationOneAsync();
+            var secondTask = GetOperationTwoAsync();
+            return await firstTask + await secondTask;
+        }
+
+
+        private static async Task<int> GetOperationOneAsync()
+        {
+            await Task.Delay(5000); // Just to simulate an operation taking time
+            Console.WriteLine("GetOperationOneAsync");
+            return 10;
+        }
+
+        private static async Task<int> GetOperationTwoAsync()
+        {
+            await Task.Delay(100); // Just to simulate an operation taking time
+            Console.WriteLine("GetOperationTwoAsync");
+            return 5;
+        }
+    }
+}
